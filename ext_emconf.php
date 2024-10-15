260f947fad579322d8ce8c15529e283f7253b247
Patrick Lenk <dev@avatara.de>
2024-10-15 13:54:03 +0200

    [TASK] Allow tt_address v9

--- ext_emconf.php
+++ ext_emconf.php
@@ -13,7 +13,7 @@ $EM_CONF[$_EXTKEY] = [
         'depends' => [
             'typo3' => '12.4.0-12.99.99',
             'lowlevel' => '12.4.0-12.99.99',
-            'tt_address' => '8.1.0-8.1.99',
+            'tt_address' => '8.1.0-9.0.99',
             'php' => '8.1.0-8.3.99',
         ],
         'conflicts' => [
